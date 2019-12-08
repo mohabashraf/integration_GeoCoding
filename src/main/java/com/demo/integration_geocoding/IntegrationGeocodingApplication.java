7f1c54cd4b18d4f7931a945be13398ab656f9d49
mohabashraf <mohabashraf466@gmail.com>
2019-12-08 09:01:49 +0200

    initial

---
+++ src/main/java/com/demo/integration_geocoding/IntegrationGeocodingApplication.java
@@ -0,0 +1,13 @@
+package com.demo.integration_geocoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationGeocodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationGeocodingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/integration_geocoding/IntegrationGeocodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.integration_geocoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationGeocodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
